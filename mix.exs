b5d91c046a1fb4fd4109eb747dc1ed19651857a8
Lincoln Fitzsimons <lincolnf@dius.com.au>
2017-03-25 15:55:52 +1100

    add httpoison

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule PhoUpload.Mixfile do
   def application do
     [mod: {PhoUpload, []},
      applications: [:phoenix, :phoenix_pubsub, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :httpoison]]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,12 +30,15 @@ defmodule PhoUpload.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:cowboy, "~> 1.0"},
+      {:gettext, "~> 0.11"},
+      {:httpoison, "~> 0.11.0"},
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:postgrex, ">= 0.0.0"}
+   ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
